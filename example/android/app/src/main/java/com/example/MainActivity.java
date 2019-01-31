785cd325f1f287a66249fc85d6d41536ffb1088a
Mike <wanggao@yuanxin2015.com>
2019-01-31 10:24:48 +0800

    demo 虚拟按钮

--- example/android/app/src/main/java/com/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Build;
 
+import android.util.Log;
+
+import android.content.res.Configuration;
+
+import android.view.Window;
+import android.view.WindowManager;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 public class MainActivity extends ReactActivity {
 
     /**
@@ -11,5 +22,43 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "example";
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(BuildConfig.DEBUG){
+            setNeedsMenuKey();//显示虚拟menu
+        }
+    }
+
+    private void setNeedsMenuKey() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            return;
+        }
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                int flags = WindowManager.LayoutParams.class.getField("FLAG_NEEDS_MENU_KEY").getInt(null);
+                getWindow().addFlags(flags);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Method setNeedsMenuKey = Window.class.getDeclaredMethod("setNeedsMenuKey", int.class);
+                setNeedsMenuKey.setAccessible(true);
+                int value = WindowManager.LayoutParams.class.getField("NEEDS_MENU_SET_TRUE").getInt(null);
+                setNeedsMenuKey.invoke(getWindow(), value);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
